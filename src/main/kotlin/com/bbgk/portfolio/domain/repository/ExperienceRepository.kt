a5fdb3185382ecba5d53a2046b6aa18165881809
ppusda <ppusda1234@gmail.com>
2024-10-12 04:16:48 +0900

    feat: 리포지토리 성능 개선

--- src/main/kotlin/com/bbgk/portfolio/domain/repository/ExperienceRepository.kt
+++ src/main/kotlin/com/bbgk/portfolio/domain/repository/ExperienceRepository.kt
@@ -2,8 +2,16 @@ package com.bbgk.portfolio.domain.repository
 
 import com.bbgk.portfolio.domain.entity.Experience
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ExperienceRepository : JpaRepository<Experience, Long> {
+    @Query("select e from Experience e left join fetch e.details where e.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Experience>
+    // fetchType = EAGER와 LAZY로 설정한 경우, 부모 조회 쿼리 + 자식의 개수 N개를 조회하는 N+1 문제가 발생
+    // 이를 해결하기 위해 join을 사용, 그 중에서도 left join을 사용하여 전체를 가져오도록 설정
+
+    @Query("select e from Experience e join fetch e.details where e.id = :id") // JPQL
+    override fun findById(id: Long): Optional<Experience>
 
 }
--- src/main/kotlin/com/bbgk/portfolio/domain/repository/ProjectRepository.kt
+++ src/main/kotlin/com/bbgk/portfolio/domain/repository/ProjectRepository.kt
@@ -2,8 +2,14 @@ package com.bbgk.portfolio.domain.repository
 
 import com.bbgk.portfolio.domain.entity.Project
 import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.data.jpa.repository.Query
+import java.util.*
 
 interface ProjectRepository : JpaRepository<Project, Long> {
+    @Query("select p from Project p left join fetch p.skills s join fetch s.skill where p.isActive = :isActive")
     fun findAllByIsActive(isActive: Boolean): List<Project>
+    // skill만 fetch join 하여 N+1 문제가 그대로인데, 이를 해결하기 위해 fetch size로 조정하여 한번에 10개의 데이터를 묶어서 가져올 수 있었음
+    @Query("select p from Project p join fetch p.details where p.id = :id")
+    override fun findById(id: Long): Optional<Project>
 
 }
