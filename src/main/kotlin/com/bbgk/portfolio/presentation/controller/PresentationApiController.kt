00734ed68863951cd58d3a54ebe3df7bcad92395
ppusda <ppusda1234@gmail.com>
2024-10-14 23:51:42 +0900

    feat: 컨트롤러 개발

--- src/main/kotlin/com/bbgk/portfolio/presentation/controller/PresentationApiController.kt
+++ src/main/kotlin/com/bbgk/portfolio/presentation/controller/PresentationApiController.kt
@@ -1,10 +1,37 @@
 package com.bbgk.portfolio.presentation.controller
 
+import com.bbgk.portfolio.presentation.dto.IntroductionDTO
+import com.bbgk.portfolio.presentation.dto.LinkDTO
+import com.bbgk.portfolio.presentation.dto.ProjectDTO
+import com.bbgk.portfolio.presentation.dto.ResumeDTO
+import com.bbgk.portfolio.presentation.service.PresentationService
+import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 @RequestMapping("/api")
-class PresentationApiController {
+class PresentationApiController(
+        private val presentationService: PresentationService
+) {
 
+    @GetMapping("/v1/introductions")
+    fun getIntroductions(): List<IntroductionDTO> {
+        return presentationService.getIntroductions()
+    }
+
+    @GetMapping("/v1/links")
+    fun getLinks(): List<LinkDTO> {
+        return presentationService.getLinks()
+    }
+
+    @GetMapping("/v1/resume")
+    fun getResume(): ResumeDTO {
+        return presentationService.getResume()
+    }
+
+    @GetMapping("/v1/projects")
+    fun getProjects(): List<ProjectDTO> {
+        return presentationService.getProjects()
+    }
 }
--- src/main/kotlin/com/bbgk/portfolio/presentation/controller/PresentationViewController.kt
+++ src/main/kotlin/com/bbgk/portfolio/presentation/controller/PresentationViewController.kt
@@ -1,4 +1,43 @@
 package com.bbgk.portfolio.presentation.controller
 
-class PresentationViewController {
+import com.bbgk.portfolio.domain.constant.SkillType
+import com.bbgk.portfolio.presentation.service.PresentationService
+import org.springframework.stereotype.Controller
+import org.springframework.ui.Model
+import org.springframework.web.bind.annotation.GetMapping
+
+@Controller
+class PresentationViewController(
+        private val presentationService: PresentationService
+) {
+
+    @GetMapping("/")
+    fun index(model: Model): String {
+        val introductions = presentationService.getIntroductions()
+        model.addAttribute("introductions", introductions)
+
+        val links = presentationService.getLinks()
+        model.addAttribute("links", links)
+
+        return "presentation/index"
+    }
+
+    @GetMapping("/resume")
+    fun resume(model: Model): String {
+        val resume = presentationService.getResume()
+        model.addAttribute("resume", resume)
+        model.addAttribute("skillTypes", SkillType.values())
+
+        return "presentation/resume"
+    }
+
+    @GetMapping("/projects")
+    fun projects(model: Model): String {
+        val projects = presentationService.getProjects()
+        model.addAttribute("projects", projects)
+
+        return "presentation/projects"
+    }
+
+
 }
