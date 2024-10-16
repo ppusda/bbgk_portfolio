d3679471930517e5e4e70e089ff2f338959f2bf7
ppusda <ppusda1234@gmail.com>
2024-10-17 01:27:46 +0900

    feat: 인터셉터 개발

--- src/main/kotlin/com/bbgk/portfolio/presentation/interceptor/PresentationInterceptor.kt
+++ src/main/kotlin/com/bbgk/portfolio/presentation/interceptor/PresentationInterceptor.kt
@@ -1,7 +1,19 @@
 package com.bbgk.portfolio.presentation.interceptor
 
+import com.bbgk.portfolio.domain.entity.HttpInterface
+import com.bbgk.portfolio.domain.repository.HttpInterfaceRepository
+import jakarta.servlet.http.HttpServletRequest
+import jakarta.servlet.http.HttpServletResponse
 import org.springframework.stereotype.Component
+import org.springframework.web.servlet.HandlerInterceptor
+import java.lang.Exception
 
 @Component
-class PresentationInterceptor {
+class PresentationInterceptor(
+        private val httpInterfaceRepository: HttpInterfaceRepository
+) : HandlerInterceptor {
+    override fun afterCompletion(request: HttpServletRequest, response: HttpServletResponse, handler: Any, ex: Exception?) {
+        val httpInterface = HttpInterface(request)
+        httpInterfaceRepository.save(httpInterface)
+    }
 }
